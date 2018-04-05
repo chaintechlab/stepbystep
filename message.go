22404158bf7efcbf274dc50ec16a8a07278c206a
Cheng Wei <chengw@seagroup.com>
2018-04-05 10:28:29 +0800

    improved message.go

--- message.go
+++ message.go
@@ -13,51 +13,99 @@ const (
 	TestNet3 = 0x0709110B
 )
 
+var (
+	CurrentMagic uint32
+)
+
 const (
 	HeaderLength     = 24
 	MaxMessageLength = 1000 * 1000 * 1000
 )
 
-type Message struct {
+type MessageHeader struct {
 	Magic    uint32
 	Cmd      string
 	Len      uint32
 	Checksum []byte
-	Payload  []byte
 }
 
-func (m *Message) Validate() error {
-	if len(m.Cmd) > 12 {
-		return errors.New("invalid cmd")
+func (h *MessageHeader) Marshal() []byte {
+	output := make([]byte, HeaderLength)
+	binary.BigEndian.PutUint32(output, h.Magic)
+	copy(output[4:16], []byte(h.Cmd))
+	binary.BigEndian.PutUint32(output[16:20], h.Len)
+	copy(output[20:24], h.Checksum)
+	return output
+}
+
+func bytesToString(input []byte) string {
+	index := bytes.IndexByte(input, 0)
+	if index == -1 {
+		return string(input)
 	}
-	if m.Len != 0 && int(m.Len) != len(m.Payload) {
-		return errors.New("invalid len")
+	return string(input[0:index])
+}
+
+func (h *MessageHeader) Unmarshal(input []byte) error {
+	if len(input) != HeaderLength {
+		return errors.New("invalid_header_length")
 	}
-	if m.Len == 0 {
-		m.Len = uint32(len(m.Payload))
+	h.Magic = binary.BigEndian.Uint32(input)
+	h.Cmd = bytesToString(input[4:16])
+	h.Len = binary.BigEndian.Uint32(input[16:20])
+	h.Checksum = input[20:24]
+	return h.validate()
+}
+
+func (h *MessageHeader) validate() error {
+	if h.Magic != CurrentMagic {
+		return errors.New("invalid_magic")
 	}
-	hash := Hash(m.Payload)
-	if m.Checksum != nil {
-		for i := 0; i < 4; i++ {
-			if m.Checksum[i] != hash[i] {
-				return errors.New("invalid hash")
-			}
+	if len(h.Cmd) > 12 {
+		return errors.New("invalid_cmd")
+	}
+	if h.Len > MaxMessageLength {
+		return errors.New("invalid_message_length")
+	}
+	return nil
+}
+
+type Message struct {
+	Header  MessageHeader
+	Payload []byte
+}
+
+func NewMessage(cmd string, payload []byte) (*Message, error) {
+	if len(cmd) > 12 {
+		return nil, errors.New("invalid_cmd")
+	}
+	if len(payload) > MaxMessageLength {
+		return nil, errors.New("invalid_message_length")
+	}
+	h := DoubleSha256(payload)
+	return &Message{
+		Header: MessageHeader{
+			Magic:    CurrentMagic,
+			Cmd:      cmd,
+			Len:      uint32(len(payload)),
+			Checksum: h[0:4],
+		},
+		Payload: payload,
+	}, nil
+}
+
+func (m *Message) validate() error {
+	hash := DoubleSha256(m.Payload)
+	for i := 0; i < 4; i++ {
+		if m.Header.Checksum[i] != hash[i] {
+			return errors.New("invalid hash")
 		}
-	} else {
-		copy(m.Checksum, hash[:4])
 	}
 	return nil
 }
 
 func (m *Message) WriteTo(w io.Writer) (n int, err error) {
-	if err := m.Validate(); err != nil {
-		return 0, err
-	}
-	output := make([]byte, HeaderLength)
-	binary.BigEndian.PutUint32(output, m.Magic)
-	copy(output[4:16], []byte(m.Cmd))
-	binary.BigEndian.PutUint32(output[16:20], m.Len)
-	copy(output[20:24], m.Checksum)
+	output := m.Header.Marshal()
 	n, err = w.Write(output)
 	if err != nil {
 		return n, err
@@ -67,35 +115,19 @@ func (m *Message) WriteTo(w io.Writer) (n int, err error) {
 }
 
 func (m *Message) ReadFrom(r io.Reader) (err error) {
-	err = binary.Read(r, binary.BigEndian, &m.Magic)
+	input := make([]byte, HeaderLength)
+	_, err = io.ReadFull(r, input)
 	if err != nil {
 		return err
 	}
-	cmdBytes := make([]byte, 12)
-	_, err = io.ReadFull(r, cmdBytes)
+	err = m.Header.Unmarshal(input)
 	if err != nil {
 		return err
 	}
-	index := bytes.IndexByte(cmdBytes, 0)
-	if index > 0 {
-		m.Cmd = string(cmdBytes[0:index])
-	} else if index == -1 {
-		m.Cmd = string(cmdBytes)
-	} else {
-		return errors.New("invalid cmd")
-	}
-	err = binary.Read(r, binary.BigEndian, &m.Len)
-	if err != nil {
-		return err
-	}
-	if m.Len > MaxMessageLength {
-		return errors.New("message is too long")
-	}
-	_, err = io.ReadFull(r, m.Checksum)
-	if err != nil {
-		return err
-	}
-	m.Payload = make([]byte, int(m.Len))
+	m.Payload = make([]byte, int(m.Header.Len))
 	_, err = io.ReadFull(r, m.Payload)
-	return err
+	if err != nil {
+		return err
+	}
+	return m.validate()
 }
--- util.go
+++ util.go
@@ -4,7 +4,7 @@ import (
 	"crypto/sha256"
 )
 
-func Hash(input []byte) [32]byte {
+func DoubleSha256(input []byte) [32]byte {
 	sum := sha256.Sum256(input)
 	return sha256.Sum256(sum[:])
 }
